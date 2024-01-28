82b1392f29728ce5aae6ab7cd6a763b6cd5cc45f
s27553-pj <s27553@pjwstk.edu.pl>
2024-01-28 15:09:38 +0100

    dodanie repozytorium

---
+++ src/main/java/com/example/s27553_bank/S27553BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s27553_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S27553BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S27553BankApplication.class, args);
+    }
+
+}

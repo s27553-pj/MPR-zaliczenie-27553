2efd9f7e3249044864acc1abbdb1f8828dfebd53
s27553-pj <s27553@pjwstk.edu.pl>
2024-01-28 15:16:40 +0100

    utworzenie modelu konta

---
+++ src/main/java/com/example/s27553_bank/Model/Account.java
@@ -0,0 +1,14 @@
+package com.example.s27553_bank.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Account {
+    private String name;
+    private String surname;
+    private Integer accountsaldo;
+    private String pesel;
+    private Currency currency;
+}
---
+++ src/main/java/com/example/s27553_bank/Model/Currency.java
@@ -0,0 +1,5 @@
+package com.example.s27553_bank.Model;
+
+public enum Currency {
+    PLN,EUR,USD
+}
